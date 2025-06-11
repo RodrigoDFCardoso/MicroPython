75bdbb8d2eefcdee64089f729f91e2354cdb591c
RodrigoCardoso <r205513@dac.unicamp.br>
2025-06-11 16:32:52 -0300

    GPS e ina226 funcionando e imprimindo no LCD

--- for_lorawan/get_dados.py
+++ for_lorawan/get_dados.py
@@ -1,72 +1,20 @@
+from machine import Pin, SoftI2C, UART
+import ssd1306
 import time
-from machine import Pin, SoftI2C, ADC, I2C, UART
-from ssd1306 import SSD1306_I2C
-import machine
-import ina226
-import uasyncio as asyncio
-import bme280_float as bme280
 
-
-
-def bme280_values():
-    i2c = machine.I2C(1, sda=machine.Pin(2), scl=machine.Pin(3))
-    bme = bme280.BME280(i2c=i2c)
-    
-    return f"Temp: {bme.values[0]}\nPres: {bme.values[1]}\nUmid: {bme.values[2]}"
-
-
-#print(bme.values)
-
-uart = UART(0,baudrate=9600, tx=Pin(0), rx=Pin(1))
-
-
-def parse_gprmc(linha):
-    try:
-        partes = linha.split(',')
-        if partes[0] == '$GPRMC' and partes[2] == 'A':  # A = válido
-            hora = partes[1]
-            lat_raw = partes[3]
-            lat_dir = partes[4]
-            lon_raw = partes[5]
-            lon_dir = partes[6]
-
-            # Converter latitude
-            lat_graus = int(lat_raw[:2])
-            lat_min = float(lat_raw[2:])
-            latitude = lat_graus + lat_min / 60.0
-            if lat_dir == 'S':
-                latitude = -latitude
-
-            # Converter longitude
-            lon_graus = int(lon_raw[:3])
-            lon_min = float(lon_raw[3:])
-            longitude = lon_graus + lon_min / 60.0
-            if lon_dir == 'W':
-                longitude = -longitude
-
-            # Converter hora
-            horas = hora[0:2]
-            minutos = hora[2:4]
-            segundos = hora[4:6]
-            horario = f"{horas}:{minutos}:{segundos} UTC"
-
-            return latitude, longitude, horario
-    except:
-        print(linha)
-        pass
-    return None, None, None
-
-
-#para BitDogLab e7.0
 # Endereço I2C do INA226
 INA226_ADDR = 0x40  # Endereço padrão do INA226
 
 # Configuração I2C para o INA226
 i2c = SoftI2C(scl=Pin(3), sda=Pin(2))  # SCL e SDA conforme seu setup
 
-#para placa BMS
-address = 68
-i2c = SoftI2C(scl=Pin(19), sda=Pin(18)) #definir qual pin i2c está
+#UART GPS NEO06m
+uart = UART(0,baudrate=9600, tx=Pin(0), rx=Pin(1))
+
+# Configuração I2C para o OLED
+oled_width = 128
+oled_height = 64
+oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
 
 # Definir registradores
 CONFIG_REG = 0x00
@@ -118,22 +66,83 @@ configurar_ina226()
 # Resistor de shunt (em ohms)
 shunt_resistor = 0.1  # 0.1 ohms
 
-while True:
-    print(bme280_values())
+# Função para exibir informações no OLED
+def exibir_no_oled(v_bus, corrente, potencia, lat, lon, hora):
+    oled.fill(0)  # Limpar o display
+    oled.text("V:{:.2f}V {}".format(v_bus, hora), 0, 0)  # Primeira linha: Tensão
+    # Corrente com sinal
+    oled.text("I: {:+.2f}mA".format(corrente), 0, 16)  # Corrente (mA) com sinal
+    oled.text("P: {:+.2f}mW".format(potencia), 0, 32)  # Potência (mW)
+    #oled.text("Shunt: {:+.3f}mV".format(ler_tensao_shunt() * -1000), 0, 48)  # Tensão de Shunt (mV)
+    oled.text("GPS:{},{}".format(lat, lon), 0, 48) 
+    oled.show()  # Atualizar o display
 
-    v_bus = ler_tensao_bus()
-    corrente = ler_corrente(shunt_resistor)
-    potencia = calcular_potencia(v_bus, corrente)
-    print(potencia)
+def parse_gprmc(linha):
+    try:
+        partes = linha.split(',')
+        if partes[0] == '$GPRMC' and partes[2] == 'A':  # A = válido
+            hora = partes[1]
+            # print(hora)
+            lat_raw = partes[3]
+            lat_dir = partes[4]
+            lon_raw = partes[5]
+            lon_dir = partes[6]
+
+            # Converter latitude
+            lat_graus = int(lat_raw[:2])
+            lat_min = float(lat_raw[2:])
+            latitude = lat_graus + lat_min / 60.0
+            #if lat_dir == 'S':
+               # latitude = -latitude
+            latitude = f'{latitude:.1f}{lat_dir}'
+            
+            # Converter longitude
+            lon_graus = int(lon_raw[:3])
+            lon_min = float(lon_raw[3:])
+            longitude = lon_graus + lon_min / 60.0
+            #if lon_dir == 'W':
+               # longitude = -longitude
+            longitude = f'{longitude:.1f}{lon_dir}'
+            # Converter hora
+            horas = hora[0:2]
+            minutos = hora[2:4]
+            segundos = hora[4:6]
+            horario = f"{horas}:{minutos}:{segundos} UTC"
+
+            return latitude, longitude, horario
+        else:
+            return None, None, None
+    except:
+        pass
+    return None, None, None
+
+def gps_signal():
     if uart.any():
         linha = uart.readline()
         if linha:
             try:
                 linha = linha.decode('utf-8').strip()
-                #print(linha)
+                print(linha)
                 lat, lon, hora = parse_gprmc(linha)
                 if lat and lon:
-                    print(f"Latitude: {lat:.6f}, Longitude: {lon:.6f}, Horário: {hora}")
+                    #print(f"Latitude: {lat:.6f}, Longitude: {lon:.6f}, Horário: {hora}")
+                    return [lat, lon, hora]
             except UnicodeError:
                 pass
-    time.sleep(2)
+
+loc = [0, 0, 0]
+# Loop principal
+while True:
+    # Ler valores
+    v_bus = ler_tensao_bus()
+    corrente = ler_corrente(shunt_resistor)
+    potencia = calcular_potencia(v_bus, corrente)
+    dados_gps = gps_signal()
+    if  dados_gps != None:
+        loc= dados_gps
+    print(loc)
+    # Exibir no OLED
+    exibir_no_oled(v_bus, corrente, potencia, loc[0], loc[1], loc[2])
+
+    time.sleep(1)
+
